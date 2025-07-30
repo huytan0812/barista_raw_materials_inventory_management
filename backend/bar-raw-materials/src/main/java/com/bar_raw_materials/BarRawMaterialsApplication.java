238f025b36c8503c6981b5f429f3b658aee0b99c
huytan0812 <huytan12c6@gmail.com>
2025-07-30 13:20:34 +0700

    Setup Spring

---
+++ backend/bar-raw-materials/src/main/java/com/bar_raw_materials/BarRawMaterialsApplication.java
@@ -0,0 +1,13 @@
+package com.bar_raw_materials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarRawMaterialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarRawMaterialsApplication.class, args);
+	}
+
+}
---
+++ backend/bar-raw-materials/src/test/java/com/bar_raw_materials/BarRawMaterialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bar_raw_materials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarRawMaterialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
