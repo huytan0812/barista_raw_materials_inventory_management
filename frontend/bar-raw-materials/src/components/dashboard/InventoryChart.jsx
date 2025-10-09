cd354b35a441de96c069a015d7fc7d40de0004ea
huytan0812 <huytan12c6@gmail.com>
2025-10-09 11:45:05 +0700

    Convert inventory value to percentage

--- frontend/bar-raw-materials/src/components/dashboard/InventoryChart.jsx
+++ frontend/bar-raw-materials/src/components/dashboard/InventoryChart.jsx
@@ -1,24 +1,58 @@
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import {Pie} from '@ant-design/charts';
+import {useAuthContext} from '../../contexts/AuthContext'
+import productInvHTTP from '../../services/ProductInventory'
 
 const InventoryChart = ({chartHeight}) => {
-    const inventoryData = [
-        { type: 'Product A', value: 40 },
-        { type: 'Product B', value: 21 },
-        { type: 'Product C', value: 17 },
-        { type: 'Product D', value: 22 },
-    ];
+    const {token} = useAuthContext();
+    const persistToken = useRef(token);
+    const [inventoryData, setInventoryData] = useState([]);
+    const [totalInventory, setTotalInventory] = useState(1);
+    
+    useEffect(() => {
+        const fetchInventoryData = async() => {
+            try {
+                const response = await productInvHTTP.get('getCurrentInventory', {
+                    headers: {
+                        Authorization: `Bearer ${persistToken.current}`
+                    },
+                    params: {
+                        limit: 5
+                    }
+                });
+                if (response.status === 200) {
+                    let calcTotalInventory = 0;
+                    for (let i = 0; i < response.data.topInventories.length; i++) {
+                        calcTotalInventory += response.data.topInventories[i].currentInventory;
+                    }
+                    const totalRemainInventories = {
+                        'productName': "Khác",
+                        'currentInventory': response.data.totalRemainInventories
+                    };
+                    calcTotalInventory += response.data.totalRemainInventories;
+                    setInventoryData([...response.data.topInventories, totalRemainInventories]);
+                    setTotalInventory(calcTotalInventory);
+                }
+            }
+            catch (error) {
+                console.log(error);
+            }
+        }
+        fetchInventoryData();
+    }, []);
 
     const inventoryConfig = {
         appendPadding: 10,
         data: inventoryData,
-        angleField: 'value',
-        colorField: 'type',
+        angleField: 'currentInventory',
+        colorField: 'productName',
         radius: 1,
         autoFit: true,
         height: chartHeight,
         label: {
-            text: 'value',
+            text: (record) => {
+                return `${Math.round(record?.currentInventory/totalInventory*100)}%`
+            },
             style: { fontSize: '1.2rem', textAlign: 'center' },
         },
     };
--- frontend/bar-raw-materials/src/components/dashboard/Tables.jsx
+++ frontend/bar-raw-materials/src/components/dashboard/Tables.jsx
@@ -1,51 +1,100 @@
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import {Row, Col, Card, List, Avatar} from 'antd'
+import {useAuthContext} from '../../contexts/AuthContext'
+import productInvHTTP from '../../services/ProductInventory'
 
 const Tables = () => {
-    const products = [
-        { name: 'Product A', qty: 500 },
-        { name: 'Product B', qty: 350 },
-        { name: 'Product C', qty: 300 },
-        { name: 'Product D', qty: 250 },
-    ];
+    const {token} = useAuthContext();
+    const persistToken = useRef(token);
+    const [topSellers, setTopSellers] = useState([]);
+    const [slowSellers, setSlowSellers] = useState([]);
+
+    // side effect for fetching top sellers
+    useEffect(() => {
+        const fetchTopSellers = async() => {
+            try {
+                const response = await productInvHTTP.get('getExportQuantity', {
+                    headers: {
+                        Authorization: `Bearer ${persistToken.current}`
+                    },
+                    params: {
+                        limit: 5,
+                        DESC: true
+                    }
+                });
+                if (response.status === 200) {
+                    setTopSellers(response.data);
+                }
+            }
+            catch (error) {
+                console.log(error);
+            }
+        }
+        fetchTopSellers();
+    }, []);
+
+    // side effect for fetching slow sellers
+    useEffect(() => {
+        const fetchSlowSellers = async() => {
+            try {
+                const response = await productInvHTTP.get('getExportQuantity', {
+                    headers: {
+                        Authorization: `Bearer ${persistToken.current}`
+                    },
+                    params: {
+                        limit: 5,
+                        DESC: false
+                    }
+                });
+                if (response.status === 200) {
+                    setSlowSellers(response.data);
+                }
+            }
+            catch (error) {
+                console.log(error);
+            }
+        }
+        fetchSlowSellers();
+    }, []);
+
     return (
         <Row gutter={16} style={{marginTop: '1.6rem'}}>
             <Col xs={24} md={8}>
             <Card title="Hàng bán chạy">
                 <List
-                dataSource={products}
-                renderItem={(item) => (
-                    <List.Item>
-                    <List.Item.Meta avatar={<Avatar>{item.name.charAt(0)}</Avatar>} title={item.name} />
-                    <div>{item.qty}</div>
-                    </List.Item>
-                )}
+                    dataSource={topSellers}
+                    renderItem={(item) => (
+                        <List.Item>
+                        <List.Item.Meta avatar={<Avatar>{item.productName?.charAt(0)}</Avatar>} title={item.productName} />
+                        <div>{item.exportQuantity}</div>
+                        </List.Item>
+                    )}
                 />
             </Card>
             </Col>
             <Col xs={24} md={8}>
             <Card title="Hàng bán chậm">
                 <List
-                dataSource={[...products].reverse()}
-                renderItem={(item) => (
-                    <List.Item>
-                    <List.Item.Meta avatar={<Avatar>{item.name.charAt(0)}</Avatar>} title={item.name} />
-                    <div>{item.qty}</div>
-                    </List.Item>
-                )}
+                    dataSource={slowSellers}
+                    renderItem={(item) => (
+                        <List.Item>
+                        <List.Item.Meta avatar={<Avatar>{item.productName?.charAt(0)}</Avatar>} title={item.productName} />
+                        <div>{item.exportQuantity}</div>
+                        </List.Item>
+                    )}
                 />
             </Card>
             </Col>
             <Col xs={24} md={8}>
             <Card title="Hàng sắp hết">
                 <List
-                dataSource={products.filter((p) => p.qty < 300)}
-                renderItem={(item) => (
-                    <List.Item>
-                    <List.Item.Meta avatar={<Avatar>{item.name.charAt(0)}</Avatar>} title={item.name} />
-                    <div>{item.qty}</div>
-                    </List.Item>
-                )}
+                    dataSource={slowSellers?.filter((p) => p.qty < 300)}
+                    renderItem={(item) => (
+                        <List.Item>
+                        <List.Item.Meta avatar={<Avatar>{item.productName?.charAt(0)}</Avatar>} title={item.productName} />
+                        <div>{item.exportQuantity}</div>
+                        </List.Item>
+                    )}
                 />
             </Card>
             </Col>
